41dd54f8cb3ea76aeffe2c300d0c8fa5d02665df
sllanos <sergio.llanos.garcia@gmail.com>
2023-05-30 07:37:08 -0500

    Change the switch on mobile devcies

--- src/components/CardIcon/CardIcon.jsx
+++ src/components/CardIcon/CardIcon.jsx
@@ -1,7 +1,9 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, useMediaQuery } from '@mui/material';
 import './CardIcon.sass';
 
 function CardIcon({ icon, name, cost, backgroundColor, handlePlanSelect, planSelected, checked }) {
+  const mobileScreen = useMediaQuery('(max-height:760px)');
+
   return (
     <div
       data-testid={`custom-card-container-${name}`}
@@ -18,12 +20,14 @@ function CardIcon({ icon, name, cost, backgroundColor, handlePlanSelect, planSel
         <Typography variant="body2" color="grey" sx={{ paddingTop: '8px' }}>
           ${checked ? cost * 10 : cost}/{checked ? 'yr' : 'mo'}
         </Typography>
-        {checked && (
+      </div>
+      {checked && (
+        <div className={`${mobileScreen ? `mobileAddition` : ''}`}>
           <Typography variant="h6" color="primary">
             2 months free
           </Typography>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
